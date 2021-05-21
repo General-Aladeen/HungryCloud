f970e3525481258c0951c36b878ad1a3763ce6ab
General-Aladeen <83897925+General-Aladeen@users.noreply.github.com>
2021-05-21 15:09:04 +0530

    Update Breathing.py

--- Code/led_status_gpio/Breathing.py
+++ Code/led_status_gpio/Breathing.py
@@ -6,41 +6,25 @@ from orangepwm import *
 gpio.init()
 
 #LED 1 (TOP RIGHT)
-led1R=port.PA0
-led1G=port.PA1
-led1B=port.PA3
+led1R=OrangePwm(100, port.PA0)
+led1G=OrangePwm(100, port.PA1)
+led1B=OrangePwm(100,port.PA3)
 #LED 2 (TOP RIGHT)
-led2R = port.PA13
-led2G = port.PA10
-led2B = port.PA2
+led2R =OrangePwm(100,port.PA13)
+led2G =OrangePwm(100,port.PA10)
+led2B =OrangePwm(100,port.PA2)
 #LED 3 (BOTTOM LEFT)
-led3R = port.PA11
-led3G = port.PA12
-led3B = port.PA6
+led3R =OrangePwm(100,port.PA11)
+led3G =OrangePwm(100,port.PA12)
+led3B =OrangePwm(100,port.PA6)
 #LED 4 (BOTTOM RIGHT)
-led4R = port.PA16
-led4G = port.PA15
-led4B = port.PA14
-
-pwm = OrangePwm(100, led1R)
-pwm = OrangePwm(100, led1G)
-pwm = OrangePwm(100, led1B)
-
-pwm = OrangePwm(100, led2R)
-pwm = OrangePwm(100, led2G)
-pwm = OrangePwm(100, led2B)
-
-pwm = OrangePwm(100, led3R)
-pwm = OrangePwm(100, led3G)
-pwm = OrangePwm(100, led3B)
-
-pwm = OrangePwm(100, led4R)
-pwm = OrangePwm(100, led4G)
-pwm = OrangePwm(100, led4B)
+led4R =OrangePwm(100,port.PA16)
+led4G =OrangePwm(100,port.PA15)
+led4B =OrangePwm(100,port.PA14)
 
 
 i=1
-pwm.start(0)
+led1R.start(0)
 try:
   while True:
     if i !=100:
