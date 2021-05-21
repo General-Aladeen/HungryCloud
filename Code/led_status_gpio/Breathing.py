90b16f93265cb6263ba826ec62fa093a3310ccf6
General-Aladeen <83897925+General-Aladeen@users.noreply.github.com>
2021-05-21 12:42:41 +0530

    Update Breathing.py

--- Code/led_status_gpio/Breathing.py
+++ Code/led_status_gpio/Breathing.py
@@ -7,8 +7,16 @@ gpio.init()
 pwm = OrangePwm(100, port.PA6)
 i=1
 pwm.start(0)
-while i!=100:
-  pwm.changeDutyCycle(i)
-  sleep(0.01)
-  i=i+1
-pwm.stop()
+while True:
+  if i !=100:
+    while i!=100:
+      pwm.changeDutyCycle(i)
+      sleep(0.01)
+      i=i+1
+  else 
+    while i!=0:
+      pwm.changeDutyCycle(i)
+      sleep(0.01)
+      i=i-1
+    
+
