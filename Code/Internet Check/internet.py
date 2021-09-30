860c990226828ed1859de96f58afe1d445f23b78
General-Aladeen <83897925+General-Aladeen@users.noreply.github.com>
2021-09-30 19:24:25 +0530

    Create internet.py

---
+++ Code/Internet Check/internet.py
@@ -0,0 +1,6 @@
+url = "http://www.kite.com"
+timeout = 5
+try:
+	request = requests.get(url, timeout=timeout)
+	print("Connected to the Internet")
+except (requests.ConnectionError, requests.Timeout) as exception:
