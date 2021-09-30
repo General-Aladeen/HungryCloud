49a71433a7a1b117b3ac8da118c60ea4d8e44df8
General-Aladeen <83897925+General-Aladeen@users.noreply.github.com>
2021-09-30 20:24:05 +0530

    Update internetloop.py

--- Code/Intern/internetloop.py
+++ Code/Intern/internetloop.py
@@ -1,7 +1,8 @@
 import urllib
-intcheck=0
-i = 0
-while i == 0:
+import time
+from itertools import count
+from multiprocessing import Process
+def intern():
     try :
         stri = "https://www.google.co.in"
         data = urllib.urlopen(stri)
@@ -9,3 +10,25 @@ while i == 0:
     except:
         intcheck = 0
     print(intcheck)
+    
+def segs():
+    print('Starting function return_zero()...')
+    intcheck = 4
+    return 0
+
+f __name__ == '__main__':
+    # counter is an infinite iterator
+    counter = count(0)
+    p1 = Process(target=intern, name='Process_inc_forever')
+    p2 = Process(target=segs, name='Process_return_zero')
+    p1.start()
+    p2.start()
+    p1.join(timeout=5)
+    p2.join(timeout=5)
+    p1.terminate()
+    p2.terminate()
+if p1.exitcode is None:
+       print(f'Oops, {p1} timeouts!')
+if p2.exitcode == 0:
+        print(f'{p2} is luck and finishes in 5 seconds!')
+
