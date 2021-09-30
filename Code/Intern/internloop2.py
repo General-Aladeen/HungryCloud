4250b3d06fdb41c2e05c7c3a81b5c79cb1bb634e
General-Aladeen <83897925+General-Aladeen@users.noreply.github.com>
2021-09-30 21:12:16 +0530

    Create internloop2.py

---
+++ Code/Intern/internloop2.py
@@ -0,0 +1,11 @@
+
+while i == 0:
+  try :
+          stri = "https://www.google.co.in"
+          data = urllib.urlopen(stri)
+          intcheck = 1
+      except:
+          intcheck = 0
+      print (intcheck)
+
+
