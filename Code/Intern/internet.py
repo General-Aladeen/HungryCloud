454ba8bbc3954c99e2fb74a791c5608cfefd58e5
General-Aladeen <83897925+General-Aladeen@users.noreply.github.com>
2021-09-30 19:36:15 +0530

    Update internet.py

--- Code/Intern/internet.py
+++ Code/Intern/internet.py
@@ -4,4 +4,5 @@ timeout = 5
 try:
 	request = requests.get(url, timeout=timeout)
 	print("Connected to the Internet")
-except (requests.ConnectionError, requests.Timeout) as exception:
+except:
+	(requests.ConnectionError, requests.Timeout) as exception
