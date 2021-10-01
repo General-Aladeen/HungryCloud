478a0b6f5fea6b78e6e1d0a286c1390e6ff0029b
General-Aladeen <83897925+General-Aladeen@users.noreply.github.com>
2021-10-01 10:12:59 +0530

    Update test.py

--- Code/Intern/test.py
+++ Code/Intern/test.py
@@ -4,8 +4,6 @@ def internet_on():
     try:
         response=urllib2.urlopen('https://pyzuri.com/',timeout=5)
 	check = 1
-        return check
     except:		
 	check = 0
-    	return check
 print(internet_on())
