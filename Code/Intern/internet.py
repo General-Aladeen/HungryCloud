7be2ccae80e6444539007f959dbe2bd3aef62f93
General-Aladeen <83897925+General-Aladeen@users.noreply.github.com>
2021-10-01 10:23:59 +0530

    Update internet.py

--- Code/Intern/internet.py
+++ Code/Intern/internet.py
@@ -1,8 +1,20 @@
 import urllib
 from socket import timeout
-try :
-    stri = "https://www.google.co.in"
-    data = urllib.urlopen(stri,timeout=20)
-    print "Connected"
-except:
-    print "not connected" 
+#try :
+#    stri = "https://www.google.co.in"
+#    data = urllib.urlopen(stri,timeout=20)
+#    print "Connected"
+#except:
+#    print "not connected" 
+import urllib2
+
+def internet_on():
+    try:
+        response=urllib2.urlopen('https://www.google.co.in',timeout=20)
+        beep = 1
+        return beep
+    except urllib2.URLError as err:
+        beep = 0
+        pass
+    return beep
+print(internet_on())
