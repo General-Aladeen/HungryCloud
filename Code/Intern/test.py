1261b8aaf88046822e3ddceace6be5d33570c40b
General-Aladeen <83897925+General-Aladeen@users.noreply.github.com>
2021-10-01 10:10:44 +0530

    Create test.py

---
+++ Code/Intern/test.py
@@ -0,0 +1,11 @@
+
+import urllib
+def internet_on():
+    try:
+        response=urllib2.urlopen('https://pyzuri.com/',timeout=5)
+	check = 1
+        return check
+    except:		
+	check = 0
+    	return check
+ print(internet_on())
