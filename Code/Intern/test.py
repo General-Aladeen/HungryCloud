635935007940b12d4d70d1a9cfa5f5620ffe9110
General-Aladeen <83897925+General-Aladeen@users.noreply.github.com>
2021-10-01 10:17:22 +0530

    Update test.py

--- Code/Intern/test.py
+++ Code/Intern/test.py
@@ -1,8 +1,8 @@
 while i == 0:
   try :
-          stri = "https://www.google.co.in"
-          data = urllib.urlopen(stri)
-          intcheck = 1
-      except:
-          intcheck = 0
-      print (intcheck)
+    stri = "https://www.google.co.in"
+    data = urllib.urlopen(stri)
+    intcheck = 1
+  except:
+    intcheck = 0
+  print (intcheck)
