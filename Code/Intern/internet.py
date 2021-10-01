c70e1062c0f6d760bb5855c0f87a416c855a61a5
General-Aladeen <83897925+General-Aladeen@users.noreply.github.com>
2021-10-01 10:19:35 +0530

    Update internet.py

--- Code/Intern/internet.py
+++ Code/Intern/internet.py
@@ -1,7 +1,7 @@
 import urllib
 try :
     stri = "https://www.google.co.in"
-    data = urllib.urlopen(stri,timeout=5)
+    data = urllib.urlopen(stri,timeout=20)
     print "Connected"
 except:
     print "not connected" 
