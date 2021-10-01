9d19c6ac91482828baf3d68d82809736ea7e5c13
General-Aladeen <83897925+General-Aladeen@users.noreply.github.com>
2021-10-01 12:26:21 +0530

    Update pin9button.py

--- Code/Buttons/pin9button.py
+++ Code/Buttons/pin9button.py
@@ -1,8 +1,14 @@
 from pyA20.gpio import gpio as GPIO
 button = Button(9)
 
-while true:
-  if button.is_pressed:
-    print('hi')
-  else:
-    print('bai')
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(9, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def Shutdown(channel):
+
+    print("Shutting Down")
+ 
+GPIO.add_event_detect(21, GPIO.FALLING, callback=Shutdown, bouncetime=2000)
+
+while 1:
+    time.sleep(1)    
