284f292320ab310c194afcbfe5bf3e3fc6f71533
General-Aladeen <83897925+General-Aladeen@users.noreply.github.com>
2021-10-04 00:47:54 +0530

    Update pin9button.py

--- Code/Buttons/pin9button.py
+++ Code/Buttons/pin9button.py
@@ -2,7 +2,7 @@ from pyA20.gpio import gpio as GPIO
 from pyA20.gpio import gpio
 from pyA20.gpio import port
 from pyA20.gpio import connector
-
+import time
 
 gpio.init()
 gpio.setcfg(port.PA19, gpio.INPUT)
@@ -10,5 +10,6 @@ gpio.setcfg(port.PA19, gpio.INPUT)
 while True:
   if gpio.input(port.PA19) == 0:
     print("ze button is pressed")
+    time.sleep(2)
 
 
